92b5965ea6654895c11a4c91392d19bff6632ebb
George <georgesash@outlook.com>
2025-04-16 10:28:04 -0500

    First commit

---
+++ ai.py
@@ -0,0 +1,355 @@
+from azure.search.documents import SearchClient
+from azure.search.documents.models import VectorizedQuery
+from azure.search.documents.indexes import SearchIndexClient
+from azure.search.documents.indexes.models import (
+    SimpleField,
+    SearchFieldDataType,
+    SearchableField,
+    SearchField,
+    VectorSearch,
+    HnswAlgorithmConfiguration,
+    VectorSearchProfile,
+    SemanticConfiguration,
+    SemanticPrioritizedFields,
+    SemanticField,
+    SemanticSearch,
+    SearchIndex,
+    AzureOpenAIVectorizer,
+    AzureOpenAIVectorizerParameters
+)
+from azure.core.credentials import AzureKeyCredential
+from openai import AzureOpenAI
+import os
+import logging  # Logging facility for Python
+from dotenv import load_dotenv, set_key
+from IPython.display import Image, display, Audio, Markdown
+import base64  # Base16, Base32, Base64, Base85 Data Encodings
+import json
+import re  # Regular expression operations
+from mimetypes import guess_type
+import uuid  # Add this import
+
+load_dotenv()
+
+search_service_endpoint = os.getenv("AZURE_SEARCH_ENDPOINT")
+search_api_key = os.getenv("AZURE_SEARCH_KEY")
+
+
+azure_endpoint = os.getenv("AZURE_OPENAI_API_ENDPOINT")
+api_key = os.getenv("AZURE_OPENAI_API_KEY")
+api_version = os.getenv("AZURE_OPENAI_API_VERSION")
+model = os.getenv('AZURE_OPENAI_MODEL')
+
+embedding_model = "text-embedding-3-small"
+
+index_name = os.getenv('AZURE_SEARCH_INDEX_NAME')
+
+env_file_path = '.env'
+
+client = AzureOpenAI(
+    api_key=api_key,
+    api_version=api_version,
+    base_url=f"{azure_endpoint}/openai/deployments/{model}",
+)
+
+embedding_client = AzureOpenAI(
+    api_key=api_key,
+    api_version=api_version,
+    base_url=f"{azure_endpoint}/openai/deployments/{embedding_model}",
+)
+
+search_client = SearchClient(
+    endpoint=search_service_endpoint,
+    index_name=index_name,
+    credential=AzureKeyCredential(search_api_key)
+)
+
+index_client = SearchIndexClient(
+    endpoint=search_service_endpoint,
+    credential=AzureKeyCredential(search_api_key)
+)
+
+index_schema = SearchIndex(
+    name=index_name,
+    fields=[
+        SimpleField(name="chunk_id", type="Edm.String", sortable=True,
+                    filterable=True, facetable=True, key=True),
+        SearchableField(name="page_content", type="Edm.String",
+                        searchable=True, retrievable=True),
+        SearchableField(name="filename",
+                        type="Edm.String", searchable=True, retrievable=True),
+        SearchableField(name="title",
+                type="Edm.String", searchable=True, retrievable=True),
+        SearchField(
+            name="contentVector",
+            type=SearchFieldDataType.Collection(SearchFieldDataType.Single),
+            searchable=True,
+            vector_search_dimensions=int(1536),
+            vector_search_profile_name="myHnswProfile",
+        )
+    ]
+)
+
+def create_vector_search():
+    vector_search = VectorSearch(
+        algorithms=[
+            HnswAlgorithmConfiguration(
+                name="myHnsw"
+            )
+        ],
+        profiles=[
+            VectorSearchProfile(
+                name="myHnswProfile",
+                algorithm_configuration_name="myHnsw",
+                vectorizer="myVectorizer"
+            )
+        ],
+        vectorizers=[
+            AzureOpenAIVectorizer(
+                name="myVectorizer",
+                azure_open_ai_parameters=AzureOpenAIVectorizerParameters(
+                    resource_uri=azure_endpoint,
+                    deployment_id=embedding_model,
+                    model_name=embedding_model,
+                    api_key=api_key
+                )
+            )
+        ]
+    )
+    return vector_search
+
+def create_index():
+    try:
+        vector_search = create_vector_search()
+        index_schema.vector_search = vector_search
+        index_client.create_index(index_schema)
+        logging.info(f"Index '{index_name}' created successfully.")
+    except Exception as e:
+        logging.error(f"Failed to create index: {e}")
+        
+def local_image_to_data_url(image_path):  # Get the url of a local image
+    mime_type, _ = guess_type(image_path)
+
+    if (mime_type is None):
+        mime_type = "application/octet-stream"
+
+    with open(image_path, "rb") as image_file:
+        base64_encoded_data = base64.b64encode(
+            image_file.read()).decode("utf-8")
+
+    return f"data:{mime_type};base64,{base64_encoded_data}"
+
+def process_img_llm(img_name):
+    client = AzureOpenAI(
+        api_key=api_key,
+        api_version=api_version,
+        base_url=f"{azure_endpoint}/openai/deployments/{model}",
+    )
+    prompt = """You are a expert math assistant. Analyze the image provided and extract any math problems.
+                Solve the math problems and provide detailed solutions.
+                If there are multiple problems, solve each one and provide solutions in a JSON format.
+                JSON Format:
+                [
+                    {
+                        "problem": "2 + 2",
+                        "solution": "The solution is 4."
+                    },
+                    {
+                        "problem": "5 * 3",
+                        "solution": "The solution is 15."
+                    }
+                ]
+            """
+    response = client.chat.completions.create(
+        model=model,
+        messages=[
+            {
+                "role": "system",
+                "content": "You are a helpful assistant to analyse images.",
+            },
+            {
+                "role": "user",
+                "content": [
+                        {"type": "text", "text": prompt},
+                        {
+                            "type": "image_url",
+                            "image_url": {"url": local_image_to_data_url(img_name)},
+                        },
+                ],
+            },
+        ],
+        max_tokens=2000,
+        temperature=0.0,
+    )
+    response_content = response.choices[0].message.content.strip()
+    logging.debug(f"Response content: {response_content}")
+
+    # Attempt to parse the response content as JSON
+    json_match = re.search(r'\{.*\}', response_content,
+                           re.DOTALL)  # Match a JSON object
+    if json_match:
+        # Extract the JSON object from the response content
+        json_str = json_match.group(0)
+        # Remove the JSON object from the response content
+        summary = response_content.replace(json_str, '').strip()
+        # Remove 'json[]' from the summary
+        summary = re.sub(r'json\s*\[\s*\]', '', summary).strip()
+        # Remove code blocks from the summary
+        summary = summary.replace('```', '').strip()
+        try:
+            response_json = json.loads(json_str)
+            logging.debug(f"Parsed JSON: {response_json}")
+            # Ensure the response contains the required keys
+            required_keys = ["problem", "solution"]
+            if not all(key in response_json for key in required_keys):
+                raise ValueError(
+                    "Response JSON does not contain all required keys")
+            # Create a formatted summary
+            formatted_summary = (
+                f"Problem: {response_json['problem']}<br>"
+                f"Solution: {response_json['solution']}<br>"
+                f"{summary}"
+            )
+        except (json.JSONDecodeError, ValueError) as e:
+            logging.error(
+                f"Failed to parse response as JSON or missing keys: {e}")
+            response_json = {
+                "error": "Failed to parse response as JSON or missing keys",
+                "response": response_content
+            }
+            formatted_summary = response_content
+    else:
+        logging.error("No JSON object found in the response content")
+        response_json = {
+            "error": "No JSON object found in the response content",
+            "response": response_content
+        }
+        formatted_summary = response_content
+
+    # Read existing data from the JSON file
+    json_filename = 'math.json'
+    try:
+        with open(json_filename, 'r') as json_file:  # Open the JSON file for reading
+            # Load the existing data from the JSON file
+            existing_data = json.load(json_file)
+    except (FileNotFoundError, json.JSONDecodeError):
+        # If the file doesn't exist or is empty, initialize existing data as an empty list
+        existing_data = []
+
+    # Append the new data to the existing data
+    if isinstance(existing_data, list):
+        existing_data.append(response_json)
+    else:
+        existing_data = [existing_data, response_json]
+
+    # Save the updated data back to the JSON file
+    with open(json_filename, 'w') as json_file:
+        json.dump(existing_data, json_file, indent=4)
+
+    return {
+        "formatted_summary": formatted_summary
+    }
+
+def format_result_for_display(result):
+    # Example: Convert the result dictionary into Markdown with LaTeX
+    formatted_result = "### Extracted Problems and Solutions:\n\n"
+    for i, item in enumerate(result, start=1):
+        problem = item.get("problem", "No problem provided")
+        solution = item.get("solution", "No solution provided")
+        formatted_result += f"**Problem {i}:**\n"
+        formatted_result += f"\\[\n{problem}\n\\]\n"
+        formatted_result += f"**Solution:**\n"
+        formatted_result += f"\\[\n{solution}\n\\]\n\n"
+    return formatted_result
+
+
+def process_img_llm_chemistry(img_name):
+    client = AzureOpenAI(
+        api_key=api_key,
+        api_version=api_version,
+        base_url=f"{azure_endpoint}/openai/deployments/{model}",
+    )
+    prompt = """You are an expert chemistry assistant. Analyze the image provided and extract any chemistry problems.
+                Solve the chemistry problems and provide detailed solutions.
+                If the problem is a multiple-choice question, first state the correct answer and then provide a detailed explanation.
+                Return the results in the following JSON format:
+                [
+                    {
+                        "problem": "What is the molar mass of H2O?",
+                        "solution": "The molar mass of H2O is 18.015 g/mol."
+                    },
+                    {
+                        "problem": "Balance the equation: H2 + O2 -> H2O",
+                        "solution": "The balanced equation is 2H2 + O2 -> 2H2O."
+                    },
+                    {
+                        "problem": "Which of the following is the molar mass of H2O? (a) 18.015 g/mol (b) 20 g/mol (c) 22 g/mol",
+                        "solution": "The correct answer is (a) 18.015 g/mol. The molar mass of H2O is calculated as follows: 2(1.008) + 15.999 = 18.015 g/mol."
+                    }
+                ]
+            """
+    response = client.chat.completions.create(
+        model=model,
+        messages=[
+            {
+                "role": "system",
+                "content": "You are a helpful assistant to analyze chemistry problems.",
+            },
+            {
+                "role": "user",
+                "content": [
+                        {"type": "text", "text": prompt},
+                        {
+                            "type": "image_url",
+                            "image_url": {"url": local_image_to_data_url(img_name)},
+                        },
+                ],
+            },
+        ],
+        max_tokens=2000,
+        temperature=0.0,
+    )
+    response_content = response.choices[0].message.content.strip()
+    logging.debug(f"Response content: {response_content}")
+
+    # Attempt to parse the response content as JSON
+    try:
+        response_json = json.loads(response_content)
+        formatted_summary = format_result_for_display(response_json.get("detailed_solutions", []))
+    except json.JSONDecodeError as e:
+        logging.error(f"Failed to parse response as JSON: {e}")
+        response_json = {"error": "Failed to parse response as JSON"}
+        formatted_summary = response_content
+
+    return {
+        "formatted_summary": formatted_summary
+    }
+
+def process_text_chemistry_problem(problem):
+    client = AzureOpenAI(
+        api_key=api_key,
+        api_version=api_version,
+        base_url=f"{azure_endpoint}/openai/deployments/{model}",
+    )
+    prompt = f"""You are an expert chemistry assistant. Solve the following problem:
+                {problem}
+                Provide a detailed solution in plain text."""
+    response = client.chat.completions.create(
+        model=model,
+        messages=[
+            {
+                "role": "system",
+                "content": "You are a helpful assistant for solving chemistry problems.",
+            },
+            {
+                "role": "user",
+                "content": prompt,
+            },
+        ],
+        max_tokens=2000,
+        temperature=0.0,
+    )
+    response_content = response.choices[0].message.content.strip()
+    return response_content
+if __name__ == "__main__":
+    create_index()
---
+++ app.py
@@ -0,0 +1,111 @@
+from flask import Flask, render_template, Response, request, send_from_directory, jsonify, redirect, url_for
+import cv2
+import os
+import json
+from azure.search.documents.models import VectorizedQuery
+from openai import AzureOpenAI
+from dotenv import load_dotenv, set_key
+from ai import process_img_llm, process_img_llm_chemistry, process_text_chemistry_problem
+
+app = Flask(__name__)
+camera = cv2.VideoCapture(0)
+capture_folder = 'captured_images'
+
+load_dotenv()
+
+azure_endpoint = os.getenv("AZURE_OPENAI_API_ENDPOINT")
+api_key = os.getenv("AZURE_OPENAI_API_KEY")
+api_version = os.getenv("AZURE_OPENAI_API_VERSION")
+model = os.getenv('AZURE_OPENAI_MODEL')
+index = os.getenv('AZURE_OPENAI_INDEX')
+
+vector_store_id = os.getenv('VECTOR_STORE_ID') 
+
+if not os.path.exists(capture_folder):
+    os.makedirs(capture_folder)
+
+client = AzureOpenAI(
+    api_key=api_key,
+    api_version=api_version,
+    base_url=f"{azure_endpoint}/openai/deployments/{model}",
+)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+def gen_frames():
+        while True:
+            success, frame = camera.read()
+            if not success:
+                break
+            else:
+                ret, buffer = cv2.imencode('.jpg', frame)
+                frame = buffer.tobytes()
+                yield (b'--frame\r\n'
+                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+    """Video streaming route. Put this in the src attribute of an img tag."""
+    return Response(gen_frames(),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/capture', methods=['POST'])
+def capture():
+    success, frame = camera.read()
+    if success:
+        img_name = os.path.join(capture_folder, "captured_image.jpg")
+        cv2.imwrite(img_name, frame)
+        result = process_img_llm(img_name)
+        return render_template('captured.html', img_name="captured_image.jpg", result=result['formatted_summary'])
+    else:
+        return "Failed to capture image"
+    
+@app.route('/process_image', methods=['POST'])
+def process_image():
+    img_name = request.json.get('img_name')
+    if not img_name:
+        return jsonify({'error': 'No image name provided'}), 400
+
+    try:
+        result = process_img_llm(img_name)
+        return jsonify(result)
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+    
+
+@app.route('/uploads/<filename>')
+def uploads(filename):
+    return send_from_directory(capture_folder, filename)
+
+@app.route('/chemistry', methods=['GET', 'POST'])
+def chemistry():
+    if request.method == 'POST':
+        action_type = request.args.get('type')
+        
+        if action_type == 'text':
+            # Handle text problem submission
+            chemistry_problem = request.form.get('chemistry_problem')
+            if chemistry_problem:
+                result = process_text_chemistry_problem(chemistry_problem)
+                return render_template('chemistry.html', img_name=None, result=result)
+        
+        elif action_type == 'capture':
+            # Handle capturing an image from the camera
+            success, frame = camera.read()
+            if success:
+                img_name = os.path.join(capture_folder, "captured_chemistry_image.jpg")
+                cv2.imwrite(img_name, frame)
+                result = process_img_llm_chemistry(img_name)
+                return render_template('chemistry.html', img_name="captured_chemistry_image.jpg", result=result['formatted_summary'])
+            else:
+                return "Failed to capture image"
+    
+    # Handle GET request
+    return render_template('chemistry.html', img_name=None, result=None)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
